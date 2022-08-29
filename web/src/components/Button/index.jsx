551bc40026efbf48acd7f419c6f34de864c0ae70
Renan Silva <renansilvaytB@gmail.com>
2022-08-29 17:27:42 -0300

    feat button component

---
+++ web/src/components/Button/index.jsx
@@ -0,0 +1,9 @@
+import { Container } from './style'
+
+export function Button({ title, ...rest }) {
+  return (
+    <Container type="button" {...rest}>
+      {title}
+    </Container>
+  )
+}
---
+++ web/src/components/Button/style.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const Container = styled.button`
+  width: 100%;
+  height: 48px;
+  background: ${({ theme }) => theme.COLORS.RED};
+  border-radius: 5px;
+  padding: 12px 32px;
+  border: 0;
+  font-family: 'Poppins', sans-serif;
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 24px;
+  color: ${({ theme }) => theme.COLORS.WHITE};
+`
