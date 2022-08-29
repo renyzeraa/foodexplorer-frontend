d1b3ff33cb9e027f1c5a7d119cece1f0b941d8f4
Renan Silva <renansilvaytB@gmail.com>
2022-08-29 16:26:18 -0300

    fix theme

---
+++ web/src/styles/theme.js
@@ -0,0 +1,18 @@
+export default {
+  COLORS: {
+    BACKGROUND_900: '#000A0F',
+    BACKGROUND_800: '#00111A',
+    BACKGROUND_700: '#0D1D25 ',
+
+    GRAY_100: '#E1E1E6',
+    GRAY_200: '#C4C4CC',
+    GRAY_300: '#999591',
+    GRAY_400: '#7C7C8A',
+
+    WHITE: '#FFFFFF',
+
+    PRICE: '#82F3FF',
+
+    RED: '#92000E'
+  }
+}
