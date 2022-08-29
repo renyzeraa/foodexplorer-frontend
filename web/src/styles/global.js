9f03a74d4f7fa56e35637d54eb1d78de3c4f1dde
Renan Silva <renansilvaytB@gmail.com>
2022-08-29 17:28:32 -0300

    feat Global Styles

---
+++ web/src/styles/global.js
@@ -0,0 +1,34 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  /* 
+  font-family: 'DM Sans', sans-serif;
+  font-family: 'Poppins', sans-serif;
+  font-family: 'Roboto', sans-serif; 
+  */
+
+  body {
+    background: ${({ theme }) => theme.COLORS.BACKGROUND_900}; 
+    color: ${({ theme }) => theme.COLORS.WHITE};  
+  }
+
+  a {
+    text-decoration: none;
+  }
+  
+  button, a {
+    cursor: pointer;
+    transition: filter 0.2s;
+  }
+
+  button:hover, a:hover {
+    filter: brightness(0.9);
+  }
+`
