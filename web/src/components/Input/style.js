2c3ba1275db28167dd6268784b5f80a33feb6c45
Renan Silva <renansilvaytB@gmail.com>
2022-08-29 16:24:57 -0300

    feat Input component

---
+++ web/src/components/Input/style.js
@@ -0,0 +1,37 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+
+  background-color: ${({ theme }) => theme.COLORS.BACKGROUND_800};
+  color: ${({ theme }) => theme.COLORS.GRAY_300};
+  border: 1px solid #ffffff;
+  border-radius: 5px;
+
+  > input {
+    font-family: 'Roboto', sans-serif;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 100%;
+
+    width: 100%;
+    height: 48px;
+
+    padding: 12px 14px;
+
+    color: ${({ theme }) => theme.COLORS.WHITE};
+    background-color: transparent;
+
+    border: 0;
+
+    &:placeholder {
+      color: ${({ theme }) => theme.COLORS.GRAY_300};
+    }
+
+    > svg {
+      margin-left: 14px;
+    }
+  }
+`
