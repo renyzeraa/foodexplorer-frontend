49295deaf57b4bba14aac328cfefa5aeb3b96560
Renan Silva <renansilvaytB@gmail.com>
2022-08-30 20:14:31 -0300

    feat Details page

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -8,12 +8,13 @@ import GlobalStyles from './styles/global'
 import { Home } from './pages/Home'
 import { SignIn } from './pages/SignIn'
 import { SignUp } from './pages/SignUp'
+import { Details } from './pages/Details'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Home />
+      <Details />
     </ThemeProvider>
   </React.StrictMode>
 )
---
+++ web/src/pages/Details/index.jsx
@@ -0,0 +1,65 @@
+import { Container } from './style'
+import { Header } from '../../components/Header'
+import { Footer } from '../../components/Footer'
+import { Button } from '../../components/Button'
+import { AiOutlineLeft, AiOutlinePlus, AiOutlineMinus } from 'react-icons/ai'
+import molla from '../../assets/pratos/molla.png'
+import rabanete from '../../assets/igredientes/rapanete.png'
+import alface from '../../assets/igredientes/alface.png'
+import paofeio from '../../assets/igredientes/paofeio.png'
+import tomate from '../../assets/igredientes/tomate.png'
+
+export function Details() {
+  return (
+    <Container>
+      <Header></Header>
+      <main>
+        <a href="">
+          <AiOutlineLeft /> voltar
+        </a>
+        <section className="content-img">
+          <img src={molla} alt="" />
+        </section>
+        <section className="content-about">
+          <h1>Salada Ravanello</h1>
+          <p>
+            Rabanetes, folhas verdes e molho agridoce salpicados com gergelim.
+          </p>
+          <div className="makings">
+            <ul>
+              <li>
+                <img src={rabanete} alt="" />
+                <p>rabanete</p>
+              </li>
+              <li>
+                <img src={alface} alt="" />
+                <p>alface</p>
+              </li>
+              <li>
+                <img src={paofeio} alt="" />
+                <p>paofeio</p>
+              </li>
+              <li>
+                <img src={tomate} alt="" />
+                <p>tomate</p>
+              </li>
+            </ul>
+          </div>
+
+          <div className="content-includes">
+            <h1 className="price-title">R$ 19,97</h1>
+            <button className="btn">
+              <AiOutlineMinus />
+            </button>
+            <span className="count-item">00</span>
+            <button className="btn">
+              <AiOutlinePlus />
+            </button>
+            <Button title="incluir"></Button>
+          </div>
+        </section>
+      </main>
+      <Footer></Footer>
+    </Container>
+  )
+}
