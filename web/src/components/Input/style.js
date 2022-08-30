2578708113aa2bd8b0e424d237f7b9aaaa537e12
Renan Silva <renansilvaytB@gmail.com>
2022-08-30 14:02:49 -0300

    fix Input style

--- web/src/components/Input/style.js
+++ web/src/components/Input/style.js
@@ -4,33 +4,31 @@ export const Container = styled.div`
   width: 100%;
   display: flex;
   align-items: center;
-
   background-color: ${({ theme }) => theme.COLORS.BACKGROUND_800};
   color: ${({ theme }) => theme.COLORS.GRAY_300};
-
+  border-radius: 10px;
+  border: 1px solid #ffffff;
   > input {
+    height: 56px;
+    width: 100%;
+    padding: 12px;
     font-family: 'Roboto', sans-serif;
     font-weight: 400;
     font-size: 16px;
     line-height: 100%;
-
-    width: 100%;
-    height: 48px;
-
-    padding: 12px 14px;
-
     color: ${({ theme }) => theme.COLORS.WHITE};
-    background-color: transparent;
-
-    border: 1px solid #ffffff;
-    border-radius: 5px;
-
-    &:placeholder {
+    background: transparent;
+    border: 0;
+    &::placeholder {
       color: ${({ theme }) => theme.COLORS.GRAY_300};
     }
-
-    > svg {
-      margin-left: 14px;
+    :focus {
+      box-shadow: 0 0 0 0;
+      border: 0 none;
+      outline: 0;
     }
+  }
+  svg {
+    margin-left: 12px;
   }
 `
