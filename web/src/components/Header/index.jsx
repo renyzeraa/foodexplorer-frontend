ba7a8cf7a1e271a9440cf8fd6fb1f69b3f9b3cc1
renyzeraa <renansilvaytb@gmail.com>
2023-05-07 10:38:52 -0300

    fix style menu header

--- web/src/components/Header/index.jsx
+++ web/src/components/Header/index.jsx
@@ -9,84 +9,75 @@ import { MdFavorite } from 'react-icons/md'
 import { TiShoppingCart } from 'react-icons/ti'
 
 export function Header({ ...rest }) {
-  function onClickMenu() {
-    document.getElementById('menu').classList.toggle('change')
-    document.getElementById('nav').classList.toggle('change')
-    document.getElementById('menu-bg').classList.toggle('change-bg')
-  }
+    function onClickMenu() {
+        document.getElementById('menu').classList.toggle('change')
+        document.getElementById('nav').classList.toggle('change')
+        document.getElementById('menu-bg').classList.toggle('change-bg')
+    }
 
-  return (
-    <Container {...rest}>
-      <main className="mobile">
-        <a href="#">
-          <img src={logo} alt="" />
-        </a>
-        <div className="input">
-          <Input
-            icon={BiSearch}
-            type="text"
-            placeholder="Busque seu prato"
-          ></Input>
-        </div>
-        <div id="menu-bar">
-          <div id="menu" onClick={onClickMenu}>
-            <div id="bar1" className="bar"></div>
-            <div id="bar2" className="bar"></div>
-            <div id="bar3" className="bar"></div>
-          </div>
-          <ul className="nav" id="nav">
-            <li>
-              <a href="#">
-                Favoritos <MdFavorite />
-              </a>
-            </li>
-            <li>
-              <a href="">
-                Compras
-                <TiShoppingCart />
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                Pedidos
-                <BiReceipt />
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                Sair
-                <GoSignOut />
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div className="menu-bg" id="menu-bg"></div>
-        <div className="menu-bg" id="menu-bg"></div>
-      </main>
+    return (
+        <Container {...rest}>
+            <main className="mobile">
+                <a href="#">
+                    <img src={logo} alt="" />
+                </a>
 
-      <main className="desktop">
-        <a href="#">
-          <img src={logo} alt="" />
-        </a>
+                <div id="menu-bar">
+                    <div id="menu" onClick={onClickMenu}>
+                        <div id="bar1" className="bar"></div>
+                        <div id="bar2" className="bar"></div>
+                        <div id="bar3" className="bar"></div>
+                    </div>
 
-        <a href="#">Meus Favoritos</a>
-        <div className="input">
-          <Input
-            icon={BiSearch}
-            type="text"
-            placeholder="Busque seu prato"
-          ></Input>
-        </div>
-        <a className="cart" href="">
-          <span className="count-itens">1</span>
-          <TiShoppingCart />
-        </a>
+                    <ul className="nav" id="nav">
+                        <div className="input">
+                            <Input
+                                icon={BiSearch}
+                                type="text"
+                                placeholder="Busque por pratos ou ingredientes"
+                            ></Input>
+                        </div>
+                        <li>
+                            <a href="#">Favoritos</a>
+                        </li>
+                        <li>
+                            <a href="">Compras</a>
+                        </li>
+                        <li>
+                            <a href="#">Pedidos</a>
+                        </li>
+                        <li>
+                            <a href="#">Sair</a>
+                        </li>
+                    </ul>
+                </div>
+                <div className="menu-bg" id="menu-bg"></div>
+                <div className="menu-bg" id="menu-bg"></div>
+            </main>
 
-        <Button icon={BiReceipt} title="Meu pedido" cont="0"></Button>
-        <a className="signout" href="#">
-          <GoSignOut />
-        </a>
-      </main>
-    </Container>
-  )
+            <main className="desktop">
+                <a href="#">
+                    <img src={logo} alt="" />
+                </a>
+
+                <a href="#">Meus Favoritos</a>
+                <div className="input">
+                    <Input
+                        icon={BiSearch}
+                        type="text"
+                        placeholder="Busque seu prato"
+                    ></Input>
+                </div>
+                <a className="cart" href="">
+                    <span className="count-itens">1</span>
+                    <TiShoppingCart />
+                </a>
+
+                <Button icon={BiReceipt} title="Meu pedido" cont="0"></Button>
+                <a className="signout" href="#">
+                    <GoSignOut />
+                </a>
+            </main>
+        </Container>
+    )
 }
--- web/src/components/Header/style.js
+++ web/src/components/Header/style.js
@@ -1,185 +1,198 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-  background: ${({ theme }) => theme.COLORS.BACKGROUND_700};
-  font-family: 'Roboto', sans-serif;
-  font-weight: 400;
-  font-size: 16px;
+    background: ${({ theme }) => theme.COLORS.BACKGROUND_700};
+    font-family: 'Roboto', sans-serif;
+    font-weight: 400;
+    font-size: 16px;
 
-  .desktop {
-    display: none;
-    opacity: 0;
-    visibility: hidden;
-  }
-
-  .mobile {
-    width: 100%;
-    height: 90px;
-    display: flex;
-    padding: 0 24px;
-    align-items: center;
-    justify-content: space-evenly;
-
-    a img {
-      width: 141px;
-      margin-right: 16px;
-    }
-
-    .input {
-      width: 400px;
-      margin-right: 60px;
-    }
-
-    #menu {
-      width: 35px;
-      height: 30px;
-      margin: 34px 24px 24px 68px;
-      cursor: pointer;
-    }
-    .bar {
-      height: 5px;
-      width: 100%;
-      background-color: #ffff;
-      display: block;
-      border-radius: 5px;
-      transition: 0.3s ease;
-    }
-    #bar1 {
-      transform: translateY(-4px);
-    }
-    #bar3 {
-      transform: translateY(4px);
-    }
-    .nav li a {
-      color: #fff;
-      text-decoration: none;
-      display: flex;
-      align-items: center;
-      justify-content: right;
-      gap: 8px;
-      font-size: 17px;
-    }
-
-    .nav li a:hover {
-      font-weight: bold;
-    }
-    .nav li {
-      list-style: none;
-      padding: 16px 0;
-    }
-    .nav {
-      padding: 0;
-      margin: 0 20px;
-      transition: 0.3s ease;
-      display: none;
-    }
-    .menu-bg,
-    #menu-bar {
-      top: 0;
-      right: 0;
-      position: absolute;
-    }
-    .menu-bg {
-      z-index: 2;
-      width: 0;
-      height: 0;
-      margin: 30px 0 20px 0;
-      background: radial-gradient(circle, #1d3f52, #000b10);
-      border-radius: 10px;
-      transition: 0.3s ease;
-      border: 1px solid #2485e938;
-    }
-    #menu-bar {
-      z-index: 3;
-    }
-    .change-bg {
-      width: 180px;
-      height: 480px;
-      transform: translate(7%, -39%);
-    }
-    .change .bar {
-      background-color: white;
-    }
-    .change #bar1 {
-      transform: translateY(4px) rotateZ(-45deg);
-    }
-    .change #bar3 {
-      transform: translateY(-6px) rotate(45deg);
-    }
-    .change #bar2 {
-      opacity: 0;
-    }
-    .change {
-      display: block;
-    }
-  }
-
-  @media (min-width: 800px) {
-    .mobile {
-      display: none;
-      opacity: 0;
-      visibility: hidden;
-    }
     .desktop {
-      opacity: 1;
-      visibility: visible;
-
-      max-width: 1120px;
-      height: 90px;
-      padding: 21px 24px;
-      display: flex;
-
-      justify-content: space-around;
-      margin: 0 auto;
-      align-items: center;
-
-      a {
-        color: ${({ theme }) => theme.COLORS.GRAY_200};
-        font-weight: 400;
-        font-size: 16px;
-      }
-
-      a img {
-        width: 141px;
-        margin-right: 16px;
-      }
-
-      .input {
-        width: 400px;
-        margin-right: 16px;
-      }
-
-      button {
-        width: 216px;
-        display: flex;
-        padding: 12px 12px 12px 0;
-      }
-
-      .cart {
-        margin: 0 16px 0 0;
-        font-size: 28px;
-        position: relative;
-        span {
-          position: absolute;
-
-          left: 18px;
-          top: -8px;
-          background-color: red;
-          border-radius: 50%;
-          font-size: 14px;
-          width: 18px;
-          height: 18px;
-
-          text-align: center;
-          color: ${({ theme }) => theme.COLORS.WHITE};
-          font-weight: bold;
-        }
-      }
-
-      .signout {
-        margin-left: 16px;
-        font-size: 24px;
-      }
+        display: none;
+        opacity: 0;
+        visibility: hidden;
     }
-  }
+
+    .mobile {
+        width: 100%;
+        height: 90px;
+        display: flex;
+        padding: 0 24px;
+        align-items: center;
+        justify-content: space-evenly;
+
+        a img {
+            width: 141px;
+            margin-right: 16px;
+        }
+
+        .input {
+            padding: 0 28px;
+            margin: 54px 0 36px;
+
+            > div {
+                background: ${({ theme }) => theme.COLORS.BACKGROUND_700};
+            }
+        }
+
+        #menu {
+            width: 35px;
+            height: 30px;
+            margin: 36px 24px 24px 24px;
+            cursor: pointer;
+        }
+        .bar {
+            height: 5px;
+            width: 100%;
+            background-color: #ffff;
+            display: block;
+            border-radius: 5px;
+            transition: 0.3s ease;
+        }
+        #bar1 {
+            transform: translateY(-4px);
+        }
+        #bar3 {
+            transform: translateY(4px);
+        }
+
+        .nav {
+            transition: 0.3s ease;
+            display: none;
+
+            li {
+                list-style: none;
+                padding: 16px 0;
+                padding-left: 28px;
+                a {
+                    color: #fff;
+                    text-decoration: none;
+                    display: flex;
+                    align-items: center;
+                    justify-content: right;
+                    gap: 8px;
+                    font-size: 17px;
+                }
+                a {
+                    color: #fff;
+                    text-decoration: none;
+                    display: flex;
+                    align-items: center;
+                    justify-content: flex-start;
+                    gap: 8px;
+                    font-size: 17px;
+                    &:hover {
+                        font-weight: bold;
+                    }
+                }
+            }
+        }
+        .menu-bg,
+        #menu-bar {
+            top: 0;
+            left: 0;
+            position: absolute;
+        }
+        .menu-bg {
+            z-index: 2;
+            width: 0;
+            height: 0;
+            background: ${({ theme }) => theme.COLORS.BACKGROUND_900};
+            transition: 0.3s ease;
+        }
+        #menu-bar {
+            z-index: 3;
+            width: 100%;
+        }
+        .change-bg {
+            width: 100vw;
+            height: 100vh;
+            transform: translate(0%, 0%);
+            box-shadow: inset 0px 86px 0px 0px
+                ${({ theme }) => theme.COLORS.BACKGROUND_700};
+        }
+        .change .bar {
+            background-color: white;
+        }
+        .change #bar1 {
+            transform: translateY(4px) rotateZ(-45deg);
+        }
+        .change #bar3 {
+            transform: translateY(-6px) rotate(45deg);
+        }
+        .change #bar2 {
+            opacity: 0;
+        }
+        .change {
+            display: block;
+        }
+    }
+
+    @media (min-width: 800px) {
+        .mobile {
+            display: none;
+            opacity: 0;
+            visibility: hidden;
+        }
+        .desktop {
+            opacity: 1;
+            visibility: visible;
+
+            max-width: 1120px;
+            height: 90px;
+            padding: 21px 24px;
+            display: flex;
+
+            justify-content: space-around;
+            margin: 0 auto;
+            align-items: center;
+
+            a {
+                color: ${({ theme }) => theme.COLORS.GRAY_200};
+                font-weight: 400;
+                font-size: 16px;
+            }
+
+            a img {
+                width: 141px;
+                margin-right: 16px;
+            }
+
+            .input {
+                width: 400px;
+                margin-right: 16px;
+            }
+
+            button {
+                width: 216px;
+                display: flex;
+                padding: 12px 12px 12px 0;
+            }
+
+            .cart {
+                margin: 0 16px 0 0;
+                font-size: 28px;
+                position: relative;
+                span {
+                    position: absolute;
+
+                    left: 18px;
+                    top: -8px;
+                    background-color: red;
+                    border-radius: 50%;
+                    font-size: 14px;
+                    width: 18px;
+                    height: 18px;
+
+                    text-align: center;
+                    color: ${({ theme }) => theme.COLORS.WHITE};
+                    font-weight: bold;
+                }
+            }
+
+            .signout {
+                margin-left: 16px;
+                font-size: 24px;
+            }
+        }
+    }
 `
