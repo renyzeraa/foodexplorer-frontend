bd8287b28e32fb9a2189ea6dbea5aa70b9a13929
renyzeraa <renansilvaytb@gmail.com>
2023-05-09 11:31:54 -0300

    fix home header mobile and desktop

--- web/src/pages/Home/style.js
+++ web/src/pages/Home/style.js
@@ -1,142 +1,152 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-  height: 100vh;
-  > main {
-    margin-top: 72px;
-    header {
-      background: linear-gradient(180deg, #091e26 0%, #00131c 100%);
-      border-radius: 10px;
-      font-family: 'Poppins', sans-serif;
-      height: 244px;
-      margin: 0 12px;
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
+    height: 100vh;
+    > main {
+        margin-top: 72px;
+        header {
+            background: linear-gradient(180deg, #091e26 0%, #00131c 100%);
+            border-radius: 10px;
+            font-family: 'Poppins', sans-serif;
+            height: 120px;
+            margin: 0 12px;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
 
-      .img-content {
-        img {
-          width: 323px;
-          position: absolute;
-          top: 210px;
-          z-index: 0;
+            .img-content {
+                img {
+                    width: 255px;
+                    position: absolute;
+                    top: 128px;
+                    left: -15px;
+                    z-index: 0;
+                }
+            }
+
+            .text-content {
+                text-align: left;
+                padding: 20px;
+                z-index: 0;
+                width: 240px;
+
+                h1 {
+                    font-weight: 500;
+                    font-size: 18px;
+                }
+                p {
+                    font-weight: 400;
+                    font-size: 12px;
+                    color: ${({ theme }) => theme.COLORS.GRAY_100};
+                }
+            }
         }
-      }
 
-      .text-content {
-        text-align: center;
-        padding: 20px;
-        z-index: 0;
-        width: 300px;
+        .content {
+            margin: 62px auto 0;
+            max-width: 1100px;
+            padding: 0 24px;
 
-        h1 {
-          font-weight: 500;
-          font-size: 40px;
+            h1 {
+                font-family: 'Poppins', sans-serif;
+                font-weight: 500;
+                font-size: 32px;
+                margin-bottom: 40px;
+            }
+
+            .cards {
+                display: flex;
+                gap: 30px;
+                overflow-x: scroll;
+                overflow-y: hidden;
+                max-width: 1100px;
+                box-shadow: 0 0 3em 0px #316661;
+                border-radius: 10px;
+                padding: 12px;
+
+                ::-webkit-scrollbar {
+                    background-color: transparent;
+                    height: 8px;
+                }
+                ::-webkit-scrollbar-track {
+                    background-color: transparent;
+                }
+                ::-webkit-scrollbar-thumb {
+                    background-color: ${({ theme }) => theme.COLORS.PRICE};
+                    border-radius: 20px;
+                }
+                ::-webkit-scrollbar-button:start:decrement {
+                    width: 50px;
+                    background-color: transparent;
+                }
+                ::-webkit-scrollbar-button:end:increment {
+                    width: 50px;
+                    background-color: transparent;
+                }
+            }
         }
-      }
+
+        .content:last-child {
+            margin-bottom: 120px;
+        }
     }
 
-    .content {
-      margin: 62px auto 0;
-      max-width: 1100px;
-      padding: 0 24px;
+    @media (min-width: 550px) {
+        main {
+            header {
+                height: 140px;
+                max-width: 1050px;
+                align-items: center;
+                margin: 0 24px;
 
-      h1 {
-        font-family: 'Poppins', sans-serif;
-        font-weight: 500;
-        font-size: 32px;
-        margin-bottom: 40px;
-      }
-
-      .cards {
-        display: flex;
-        gap: 30px;
-        overflow-x: scroll;
-        overflow-y: hidden;
-        max-width: 1100px;
-        box-shadow: 0 0 3em 0px #316661;
-        border-radius: 10px;
-        padding: 12px;
-
-        ::-webkit-scrollbar {
-          background-color: transparent;
-          height: 8px;
+                .img-content {
+                    img {
+                        width: 341px;
+                        position: absolute;
+                        top: 94px;
+                    }
+                }
+                .text-content {
+                    text-align: left;
+                    width: 355px;
+                }
+            }
         }
-        ::-webkit-scrollbar-track {
-          background-color: transparent;
-        }
-        ::-webkit-scrollbar-thumb {
-          background-color: ${({ theme }) => theme.COLORS.PRICE};
-          border-radius: 20px;
-        }
-        ::-webkit-scrollbar-button:start:decrement {
-          width: 50px;
-          background-color: transparent;
-        }
-        ::-webkit-scrollbar-button:end:increment {
-          width: 50px;
-          background-color: transparent;
-        }
-      }
     }
 
-    .content:last-child {
-      margin-bottom: 120px;
-    }
-  }
-
-  @media (min-width: 550px) {
-    main {
-      header {
-        max-width: 1050px;
-        align-items: center;
-        margin: 0 24px;
-
-        .img-content {
-          img {
-            width: 460px;
-            position: absolute;
-            top: 127px;
-            z-index: 0;
-          }
+    @media (min-width: 800px) {
+        main {
+            margin-top: 124px;
+            header {
+                height: 260px;
+                .img-content {
+                    position: relative;
+                    img {
+                        width: 600px;
+                        top: -235px;
+                        left: -62px;
+                    }
+                }
+                .text-content {
+                    text-align: left;
+                    width: auto;
+                    h1 {
+                        font-size: 40px;
+                    }
+                    p {
+                        font-size: 16px;
+                        color: ${({ theme }) => theme.COLORS.GRAY_300};
+                    }
+                }
+            }
         }
-        .text-content {
-          text-align: left;
-          width: auto;
-        }
-      }
     }
-  }
 
-  @media (min-width: 800px) {
-    main {
-      margin-top: 124px;
-      header {
-        margin: 0 24px;
-        .img-content {
-          img {
-            width: 556px;
-            top: 120px;
-          }
+    @media (min-width: 1100px) {
+        main {
+            header {
+                margin: 0 auto;
+            }
         }
-        .text-content {
-          text-align: left;
-          width: auto;
-          p {
-            font-weight: 400;
-            font-size: 16px;
-            color: ${({ theme }) => theme.COLORS.GRAY_300};
-          }
-        }
-      }
     }
-  }
-
-  @media (min-width: 1100px) {
-    main {
-      header {
-        margin: 0 auto;
-      }
-    }
-  }
 `
