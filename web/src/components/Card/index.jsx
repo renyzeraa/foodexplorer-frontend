ec1478ed9e55ab7d1fdd74116d2f378e4f1147cc
renyzeraa <renansilvaytb@gmail.com>
2023-05-09 16:23:59 -0300

    fix card to new layout

--- web/src/components/Card/index.jsx
+++ web/src/components/Card/index.jsx
@@ -1,34 +1,62 @@
 import { Container } from './style'
 import {
-  AiOutlineHeart,
-  AiFillHeart,
-  AiOutlinePlus,
-  AiOutlineMinus
+    AiOutlineHeart,
+    AiFillHeart,
+    AiOutlinePlus,
+    AiOutlineMinus
 } from 'react-icons/ai'
+import { TbPencil } from 'react-icons/tb'
 import { Button } from '../Button'
+import { Link } from 'react-router-dom'
 
-export function Card({ title, price, img, description, ...rest }) {
-  return (
-    <Container {...rest}>
-      <button className="btn-fav ">
-        <AiOutlineHeart />
-      </button>
-      <div className="container">
-        <img src={img} alt="" />
-        <h1 className="product-title">{title}</h1>
-        <p className="description">{description}</p>
-        <h1 className="price-title">R$ {price}</h1>
-        <div className="content-includes">
-          <button className="btn">
-            <AiOutlineMinus />
-          </button>
-          <span className="count-item">00</span>
-          <button className="btn">
-            <AiOutlinePlus />
-          </button>
-          <Button title="incluir"></Button>
-        </div>
-      </div>
-    </Container>
-  )
+export function Card({
+    title,
+    price,
+    img,
+    description,
+    onFavorite,
+    isAdmin,
+    ...rest
+}) {
+    const favorite = false
+
+    function handleFavorite() {}
+
+    return (
+        <Container {...rest}>
+            <button className="btn-fav " onClick={handleFavorite}>
+                {isAdmin ? (
+                    <Link>
+                        <TbPencil />
+                    </Link>
+                ) : favorite ? (
+                    <AiFillHeart />
+                ) : (
+                    <AiOutlineHeart />
+                )}
+            </button>
+            <div className="container">
+                <img src={img} alt="" />
+                <h1 className="product-title">{title}</h1>
+                <p className="description">{description}</p>
+                <h1 className="price-title">R$ {price}</h1>
+                <div className="content-includes">
+                    {isAdmin ? (
+                        []
+                    ) : (
+                        <>
+                            <button className="btn">
+                                <AiOutlineMinus />
+                            </button>
+                            <span className="count-item">00</span>
+                            <button className="btn">
+                                <AiOutlinePlus />
+                            </button>
+                            <Button title="incluir"></Button>
+                        </>
+                    )}
+                </div>
+            </div>
+        </Container>
+    )
 }
