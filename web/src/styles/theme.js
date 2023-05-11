189a8ed43d1f2f140cd19ec9169ffb6def2048ab
renyzeraa <renansilvaytb@gmail.com>
2023-05-11 19:16:40 -0300

    feat new colors

--- web/src/styles/theme.js
+++ web/src/styles/theme.js
@@ -5,9 +5,11 @@ export default {
         BACKGROUND_800: '#00111A',
         BACKGROUND_750: '#1E1E1E',
         BACKGROUND_700: '#0D1D25',
+        BACKGROUND_INPUT: '#0D161B',
         BACKGROUND_CARD: '#0b4e7038',
         BACKGROUND_INGREDIENT: '#192227',
-        
+        BACKGROUND_TAGS: '#76797B',
+
         GRAY_100: '#E1E1E6',
         GRAY_200: '#C4C4CC',
         GRAY_300: '#999591',
