ea7e4316629c86ce9a71f5b76a8969e8953c490b
renyzeraa <renansilvaytb@gmail.com>
2023-05-23 19:58:56 -0300

    feat services api

--- web/src/services/api.js
+++ web/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const api = axios.create({
-    baseUrl: 'http://localhost:3333'
+    baseURL: 'http://localhost:3333'
 })
-api.get('/users/:id')
+api.get('/users')
