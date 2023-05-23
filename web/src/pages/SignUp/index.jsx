c6895047fa9693b2cbb3e19089c330df65e4dc1b
renyzeraa <renansilvaytb@gmail.com>
2023-05-23 20:31:28 -0300

    feat email, password, name check

--- web/src/pages/SignUp/index.jsx
+++ web/src/pages/SignUp/index.jsx
@@ -3,8 +3,38 @@ import logo from '../../assets/logo.svg'
 import { Input } from '../../components/Input'
 import { Button } from '../../components/Button'
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
+
+import { api } from '../../services/api'
 
 export function SignUp() {
+    const [name, setName] = useState()
+    const [email, setEmail] = useState()
+    const [password, setPassword] = useState()
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+    function handleSignUp() {
+        if (!name || !email || !password) {
+            return alert('Por favor, preenche todos os campos !')
+        }
+
+        if (!emailRegex.test(email)) {
+            return alert('Preencha um email válido!')
+        }
+
+        api.post('/users', { name, email, password })
+            .then(() => {
+                alert('Usuário cadastrado com sucesso')
+            })
+            .catch(error => {
+                if (error.response) {
+                    alert(error.response.data.message)
+                } else {
+                    alert('Não foi possível cadastrar este usuário')
+                }
+            })
+        console.log(name, email, password)
+    }
+
     return (
         <Container>
             <main>
@@ -18,19 +48,22 @@ export function SignUp() {
                         <Input
                             placeholder="Exemplo: João de Souza"
                             type="text"
+                            onChange={e => setName(e.target.value)}
                         />
                         <p htmlFor="">Email</p>
                         <Input
                             placeholder="Exemplo: exemplo@exemplo.com"
                             type="text"
+                            onChange={e => setEmail(e.target.value)}
                         />
                         <p>Senha</p>
                         <Input
                             placeholder="No mínimo 6 caracteres"
                             type="password"
+                            onChange={e => setPassword(e.target.value)}
                         />
 
-                        <Button title="Criar Conta" />
+                        <Button title="Criar Conta" onClick={handleSignUp} />
 
                         <Link to="/">Já tenho uma conta</Link>
                     </Form>
