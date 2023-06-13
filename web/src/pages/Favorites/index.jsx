d795613dbabc13b75b309389b710a1ee76cabfdb
renyzeraa <renansilvaytb@gmail.com>
2023-06-13 18:09:10 -0300

    fix route and Favorite page

---
+++ web/src/pages/Favorites/index.jsx
@@ -0,0 +1,9 @@
+import { Container } from './style'
+
+export function Favorites() {
+  return (
+    <Container>
+      <h1>teste</h1>
+    </Container>
+  )
+}
--- web/src/routes/app.routes.jsx
+++ web/src/routes/app.routes.jsx
@@ -5,17 +5,20 @@ import { Payment } from '../pages/Payment'
 import { Demand } from '../pages/Demand'
 import { DemandAdmin } from '../pages/DemandAdmin'
 import { Plate } from '../pages/Plate'
+import { EditPlate } from '../pages/EditPlate'
+import { Favorites } from '../pages/Favorites'
 
 export function AppRoutes() {
-    return (
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/details" element={<Details />} />
-            <Route path="/payment" element={<Payment />} />
-            <Route path="/demand" element={<Demand />} />
-            <Route path="/demandadmin" element={<DemandAdmin />} />
-            <Route path="/plates" element={<Plate />} />
-            {/* <Route path="/favorites" element={<Favorites />} /> */}
-        </Routes>
-    )
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/details" element={<Details />} />
+      <Route path="/payment" element={<Payment />} />
+      <Route path="/demand" element={<Demand />} />
+      <Route path="/demandadmin" element={<DemandAdmin />} />
+      <Route path="/plates" element={<Plate />} />
+      <Route path="/plates/:id" element={<EditPlate />} />
+      <Route path="/favorites" element={<Favorites />} />
+    </Routes>
+  )
 }
