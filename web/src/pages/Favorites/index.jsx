cb899cdee548c88e6825e2af9087ccd7319f8511
renyzeraa <renansilvaytb@gmail.com>
2023-06-24 18:11:03 -0300

    Feat Page Plates Favorites

--- web/src/pages/Favorites/index.jsx
+++ web/src/pages/Favorites/index.jsx
@@ -1,9 +1,31 @@
+import { Link } from 'react-router-dom'
+import { Footer } from '../../components/Footer'
+import { Header } from '../../components/Header'
 import { Container } from './style'
+import { AiOutlineLeft } from 'react-icons/ai'
+import { PlateFav } from '../../components/PlateFav'
 
 export function Favorites() {
   return (
     <Container>
-      <h1>teste</h1>
+      <Header></Header>
+      <section className="content">
+        <header className="content-header">
+          <h1 className="title-page">Meus favoritos</h1>
+          <Link to="/">
+            <AiOutlineLeft /> voltar
+          </Link>
+        </header>
+        <main className="content-favorites">
+          <PlateFav title="Feijão Com Arroz" plateId="1"></PlateFav>
+          <PlateFav title="Pipoca Salgada"></PlateFav>
+          <PlateFav title="Canjica"></PlateFav>
+          <PlateFav title="Peixe Tofu"></PlateFav>
+          <PlateFav title="Salada Ravello"></PlateFav>
+          <PlateFav title="Pipoca Doce"></PlateFav>
+        </main>
+      </section>
+      <Footer></Footer>
     </Container>
   )
 }
--- web/src/pages/Favorites/style.js
+++ web/src/pages/Favorites/style.js
@@ -1,3 +1,53 @@
 import styled from 'styled-components'
 
-export const Container = styled.div``
+export const Container = styled.div`
+  width: 100%;
+
+  .content {
+    height: 70vh;
+    padding: 0 24px;
+    margin-top: 48px;
+    margin-bottom: 64px;
+
+    .content-header {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      margin-bottom: 24px;
+
+      .title-page {
+        font-size: 32px;
+        font-family: 'Poppins', sans-serif;
+        font-weight: 500;
+        line-height: 140%;
+      }
+
+      a {
+        display: flex;
+        align-items: center;
+        font-weight: 500;
+        font-size: 16px;
+        font-family: 'Poppins', sans-serif;
+        color: ${({ theme }) => theme.COLORS.GRAY_200};
+      }
+    }
+    .content-favorites {
+      display: grid;
+    }
+  }
+
+  @media (min-width: 768px) {
+    .content {
+      max-width: 1120px;
+      margin: 34px auto 64px;
+
+      .content-favorites {
+        display: flex;
+        justify-content: flex-start;
+        flex-direction: row;
+        flex-wrap: wrap;
+        gap: 50px 12px;
+      }
+    }
+  }
+`
