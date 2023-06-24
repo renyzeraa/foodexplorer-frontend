3943a94c1f41755691d7b96a58d7c66b86656000
renyzeraa <renansilvaytb@gmail.com>
2023-06-24 18:51:38 -0300

    feat load fav plates

--- web/src/pages/Favorites/index.jsx
+++ web/src/pages/Favorites/index.jsx
@@ -4,10 +4,32 @@ import { Header } from '../../components/Header'
 import { Container } from './style'
 import { AiOutlineLeft } from 'react-icons/ai'
 import { PlateFav } from '../../components/PlateFav'
+import { api } from '../../services/api'
+import { useEffect, useState } from 'react'
+import { Loading } from '../../components/Loading'
 
 export function Favorites() {
+  const [loading, setLoading] = useState(false)
+  const [platesFav, setPlatesFav] = useState([])
+
+  useEffect(() => {
+    async function searchPlatesFav() {
+      const response = await api.get('/favorites/favorite_plates')
+      setPlatesFav(response.data)
+    }
+    try {
+      setLoading(true)
+      searchPlatesFav()
+      setLoading(false)
+    } catch (error) {
+      setLoading(false)
+      console.error(error.message)
+    }
+  }, [])
+
   return (
     <Container>
+      {loading && <Loading></Loading>}
       <Header></Header>
       <section className="content">
         <header className="content-header">
