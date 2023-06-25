2933bdbd33c554bc0019136d958e54ad785eb41f
renyzeraa <renansilvaytb@gmail.com>
2023-06-25 14:55:29 -0300

    fix name propriety

--- web/src/components/Button/index.jsx
+++ web/src/components/Button/index.jsx
@@ -1,11 +1,11 @@
 import { Container } from './style'
 
-export function Button({ icon: Icon, title, cont, ...rest }) {
+export function Button({ icon: Icon, title, count, ...rest }) {
   return (
     <Container type="button" {...rest}>
       {Icon && <Icon size={20} />}
       {title}
-      {cont && ' (0)'}
+      {count && ` (${count})`}
     </Container>
   )
 }
