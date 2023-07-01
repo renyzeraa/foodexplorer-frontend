74a7952353d99f509e778c7de72f964ac8ebb59f
renyzeraa <renansilvaytb@gmail.com>
2023-06-30 21:20:34 -0300

    feat shopping cart

---
+++ web/src/components/ShoppingCart/index.jsx
@@ -0,0 +1,49 @@
+import { createContext, useState } from 'react'
+
+export const CartContext = createContext()
+
+export default function CartProvider({ children }) {
+  const [productsCart, setProductsCart] = useState([])
+
+  function addProductToCart(id) {
+    const copyProductsCart = [...productsCart]
+
+    const item = copyProductsCart.find(product => product.id === id)
+
+    if (!item) {
+      copyProductsCart.push({ id: id, qtd: 1 })
+    } else {
+      item.qtd = item.qtd + 1
+    }
+
+    setProductsCart(copyProductsCart)
+  }
+
+  function removeProductToCart(id) {
+    const copyProductsCart = [...productsCart]
+
+    const item = copyProductsCart.find(product => product.id === id)
+
+    if (item && item.qtd > 1) {
+      item.qtd = item.qtd - 1
+      setProductsCart(copyProductsCart)
+    } else {
+      const arrayFiltered = copyProductsCart.filter(
+        product => product.id !== id
+      )
+      setProductsCart(arrayFiltered)
+    }
+  }
+
+  function clearCart() {
+    setProductsCart([])
+  }
+
+  return (
+    <CartContext.Provider
+      value={{ productsCart, addProductToCart, removeProductToCart, clearCart }}
+    >
+      {children}
+    </CartContext.Provider>
+  )
+}
