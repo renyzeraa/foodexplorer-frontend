9116766591b01b9dab6e6549f280d6e02191e7c1
renyzeraa <renansilvaytb@gmail.com>
2023-07-11 18:34:35 -0300

    feat EnterKey to login

--- web/src/pages/SignIn/index.jsx
+++ web/src/pages/SignIn/index.jsx
@@ -7,43 +7,50 @@ import { Link } from 'react-router-dom'
 import { useAuth } from '../../hooks/auth'
 
 export function SignIn() {
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const { signIn } = useAuth()
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const { signIn } = useAuth()
 
-    function handleSignIn() {
-        signIn({ email, password })
+  function handleSignIn() {
+    signIn({ email, password })
+  }
+
+  function handleEnterInput(event) {
+    if (event.key === 'Enter') {
+      signIn({ email, password })
     }
+  }
 
-    return (
-        <Container>
-            <main>
-                <div className="content-logo">
-                    <img src={logo} alt="logo foodexplorer" />
-                </div>
-                <div className="content-login">
-                    <Form>
-                        <h1>Faça login</h1>
+  return (
+    <Container>
+      <main>
+        <div className="content-logo">
+          <img src={logo} alt="logo foodexplorer" />
+        </div>
+        <div className="content-login">
+          <Form>
+            <h1>Faça login</h1>
 
-                        <p htmlFor="">Email</p>
-                        <Input
-                            placeholder="Exemplo: exemplo@exemplo.com"
-                            type="text"
-                            onChange={e => setEmail(e.target.value)}
-                        />
-                        <p>Senha</p>
-                        <Input
-                            placeholder="No mínimo 6 caracteres"
-                            type="password"
-                            onChange={e => setPassword(e.target.value)}
-                        />
+            <p htmlFor="">Email</p>
+            <Input
+              placeholder="Exemplo: exemplo@exemplo.com"
+              type="text"
+              onChange={e => setEmail(e.target.value)}
+            />
+            <p>Senha</p>
+            <Input
+              placeholder="No mínimo 6 caracteres"
+              type="password"
+              onChange={e => setPassword(e.target.value)}
+              onKeyDown={handleEnterInput}
+            />
 
-                        <Button title="Entrar" onClick={handleSignIn} />
+            <Button title="Entrar" onClick={handleSignIn} />
 
-                        <Link to="/signup">Criar uma conta</Link>
-                    </Form>
-                </div>
-            </main>
-        </Container>
-    )
+            <Link to="/signup">Criar uma conta</Link>
+          </Form>
+        </div>
+      </main>
+    </Container>
+  )
 }
