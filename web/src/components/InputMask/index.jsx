d42780a91e3eaa1ae82ab98bb3b5e9392e9e1fad
renyzeraa <renansilvaytb@gmail.com>
2023-07-11 21:10:06 -0300

    feat input mask

--- web/src/components/InputMask/index.jsx
+++ web/src/components/InputMask/index.jsx
@@ -5,12 +5,18 @@ export function InputMask({
   Icon = false,
   sMask = '',
   placeHolder = '',
+  maskPlaceholder = '',
   ...rest
 }) {
   return (
     <Container>
       {Icon && <Icon size={20} />}
-      <InputWithMask placeholder={placeHolder} mask={sMask} {...rest} />
+      <InputWithMask
+        placeholder={placeHolder}
+        mask={sMask}
+        maskChar={maskPlaceholder}
+        {...rest}
+      />
     </Container>
   )
 }
