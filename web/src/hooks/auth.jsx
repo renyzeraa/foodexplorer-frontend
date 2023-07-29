667e8717894a017df7cdb0c7a9971c55663fc76e
Renan Silva <renansilvaytB@gmail.com>
2023-07-29 12:06:16 -0300

    feat first user is admin

--- web/src/hooks/auth.jsx
+++ web/src/hooks/auth.jsx
@@ -18,6 +18,10 @@ function AuthProvider({ children }) {
       const response = await api.post('/sessions', { email, password })
       const { user, token } = response.data
 
+      if (user.id == 1) {
+        user.isAdmin = true
+      }
+
       localStorage.setItem('@foodexplorer:user', JSON.stringify(user))
       localStorage.setItem('@foodexplorer:token', token)
 
