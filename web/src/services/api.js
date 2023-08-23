8cfee27aa694d9c9b3f62056d9a89a524cd3990e
Renan Silva <renansilvaytB@gmail.com>
2023-08-22 21:47:24 -0300

    fix api url

--- web/src/services/api.js
+++ web/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'https://foodexplorer-new-backend.onrender.com' // para produção
+  baseURL: 'https://foodexplorer-new-backend.onrender.com/' // para produção
   // baseURL : 'http://localhost:3333/' // para teste
 })
